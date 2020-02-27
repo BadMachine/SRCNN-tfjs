3cbd201f34fd609849ce08a4d5eb18eeace31e0b
Roman <iskullbreakeri@gmail.com>
2020-02-27 16:11:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const prediction = require("./src/Predict_network.js");
+const training = require("./src/Train_network.js");
+const predict = require("./src/Predict_on_own.js");
+
+
+prediction.predict();
+//let test = training.train({ createDataset: false });
+
+
+//let test = predict.predict_on_image();
